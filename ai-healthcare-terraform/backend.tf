6274942f8bab8becda520dab926aae710668e363
Keerthan2006 <keerthannandimandalam@gmail.com>
2026-09-20 20:50:54 +0530

    Add Terraform infrastructure and CI/CD

--- ai-healthcare-terraform/backend.tf
+++ ai-healthcare-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "ai-healthcare-tfstate-bucket2006"
+    bucket       = "ai-healthcare-tfstate-bucket0404"
     key          = "ai-healthcare-platform/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
